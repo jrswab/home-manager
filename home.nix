766cd2eead2047ec57a79b12ea6d371ce4e52078
Jaron Swab <jr@jrswab.com>
2025-03-24 09:53:57 -0400

    fix indentation

--- home.nix
+++ home.nix
@@ -1,21 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "jaron";
-  home.homeDirectory = "/home/jaron";
-  home.stateVersion = "24.11";
+	home.username = "jaron";
+	home.homeDirectory = "/home/jaron";
+	home.stateVersion = "24.11";
 
-  home.packages = [
-		pkgs.zsh
-		pkgs.tmux
-		pkgs.neovim
+	home.packages = [
+	pkgs.zsh
+	pkgs.tmux
+	pkgs.neovim
 
-		pkgs.brave
-		pkgs.thunderbird
-		# pkgs.syncthing # needs nixOS system ability
-  ];
+	pkgs.brave
+	pkgs.thunderbird
+	# pkgs.syncthing # needs nixOS system ability
+	];
 
-  home.file = {
+	home.file = {
 		# name = {}
 		# Where name is the path of the target directory/file(s)
 		# If name is not the target path "target = path;" must be added
@@ -37,24 +37,24 @@
 		".zshrc" = {
 			source = ./zshrc;
 		};
-  };
+	};
 
-  # These will be explicitly sourced when using a shell provided by Home Manager.
-  home.sessionVariables = {
-    EDITOR = "nvim";
+	# These will be explicitly sourced when using a shell provided by Home Manager.
+	home.sessionVariables = {
+		EDITOR = "nvim";
 		XDG_CONFIG_HOME = "$HOME/.config";
 		XDG_CACHE_HOME = "$HOME/.cache";
 		XDG_DATA_HOME = "$HOME/.local/share";
 		SSH_KEY_PATH = "$HOME/.ssh/id_ed25519";
-  };
+	};
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 
 	# Let Home Manager configure Git.
 	programs.git = {
-    enable = true;
-    userName = "Jaron Swab";
-    userEmail = "jr@jrswab.com";
-  };
+		enable = true;
+		userName = "Jaron Swab";
+		userEmail = "jr@jrswab.com";
+	};
 }
